2d10296f74cc3801a0cbf2013787b904e62153f4
HilkiasPF <101778875+HilkiasPF@users.noreply.github.com>
2022-04-28 13:38:26 -0300

    test2

--- email.py
+++ email.py
@@ -6,11 +6,8 @@ count = 0
 for c in data:
     email[c] = (f'{c}{count}@email.com')
     count += 1
-
 print(email)
-csv_file = 'email.csv'
 
 w = csv.writer(open('email.csv','w'))
-
 for c, n in email.items():
     w.writerow([c, n])
